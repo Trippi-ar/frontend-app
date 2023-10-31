bd085137cf7c25bafa46dcfba89fffd19d5482ef
EmiCussino <1006633@ucc.edu.ar>
2023-10-31 12:47:42 -0300

    se agrego modelo para filtros

---
+++ trippi_app/src/components/commons/Filtros.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+
+export default function Filtros() {
+    return (
+        <Box sx={{ minWidth: 120 , display: "flex", marginTop: 5}}>
+            <FormControl fullWidth>
+                <InputLabel variant="outlined" htmlFor="uncontrolled-native">
+                    Distancia
+                </InputLabel>
+                <NativeSelect
+                    defaultValue={30}
+                    inputProps={{
+                        name: 'distancia',
+                        id: 'uncontrolled-native',
+                    }}
+                >
+                    <option value={1}>Cerca</option>
+                    <option value={2}>Lejos</option>
+                    <option value={3}>Muy Lejos</option>
+                </NativeSelect>
+            </FormControl>
+            <FormControl fullWidth>
+                <InputLabel variant="outlined" htmlFor="uncontrolled-native">
+                    Valoracion
+                </InputLabel>
+                <NativeSelect
+                    defaultValue={30}
+                    inputProps={{
+                        name: 'valoracion',
+                        id: 'uncontrolled-native',
+                    }}
+                >
+                    <option value={1}>Bueno</option>
+                    <option value={2}>Medio</option>
+                    <option value={3}>Malo</option>
+                </NativeSelect>
+            </FormControl>
+            <FormControl fullWidth>
+                <InputLabel variant="outlined" htmlFor="uncontrolled-native">
+                    Costo
+                </InputLabel>
+                <NativeSelect
+                    defaultValue={30}
+                    inputProps={{
+                        name: 'Costo',
+                        id: 'uncontrolled-native',
+                    }}
+                >
+                    <option value={1}>Caro</option>
+                    <option value={2}>Medio</option>
+                    <option value={3}>Barato</option>
+                </NativeSelect>
+            </FormControl>
+        </Box>
+    );
+}
--- trippi_app/src/components/commons/Header.js
+++ trippi_app/src/components/commons/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import logo from "../images/Logo.png";
 
-import { AppBar, Toolbar , makeStyles} from "@mui/material";
+import { AppBar, Toolbar , makeStyles, TextField} from "@mui/material";
 import { Search } from "@mui/icons-material";
+import {Login} from "@mui/icons-material";
 import { ThemeContext } from "@emotion/react";
+import Filtros from "./Filtros";
 const Header = () => {
 
     return (
@@ -11,8 +13,11 @@ const Header = () => {
         <AppBar>
             <Toolbar >
                 <img src={logo} ></img>
+                <TextField id="outlined-basic" label="Buscar Aqui..." variant="outlined" />
                 <Search/>
+                <Login/>
             </Toolbar>
+            <Filtros/>
 
 
         </AppBar>
