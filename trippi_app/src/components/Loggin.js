f3953e85e42972e6e0d26975f91dfe2ede510505
EmiCussino <1006633@ucc.edu.ar>
2023-11-05 12:51:32 -0300

    cambios en loggin

--- trippi_app/src/components/Loggin.js
+++ trippi_app/src/components/Loggin.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React,  { useState }from 'react';
 
 function Loggin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Aquí puedes agregar la lógica de autenticación, por ejemplo, enviar los datos al servidor.
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
     <div>
-      
-      <h1>Loggin</h1>
-      <p>Welcome to loggin data!</p>
-      {/* Add any other content or links here */}
+      <h2>Iniciar sesión</h2>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="text" value={email} onChange={handleEmailChange} />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Iniciar sesión
+        </button>
+      </form>
     </div>
   );
+
 }
 
 export default Loggin;
