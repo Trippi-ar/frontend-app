0752c51921b0bc7c7ef42b62518169abaeb95ee0
Tomas Aparicio <tomyaparicio99@gmail.com>
2023-11-05 22:56:06 -0300

    se hizo login

--- trippi_app/src/components/Loggin.js
+++ trippi_app/src/components/Loggin.js
@@ -1,8 +1,12 @@
-import React,  { useState }from 'react';
+import React, { useState } from 'react';
+import Header from './commons/Header';
+import './loggin.css'; // Importa tu archivo CSS aquíi
+import Logo from './images/Logo2.png'
+import { AppBar, SvgIcon} from "@mui/material";
 
-import { IconButton } from '@mui/material';
 
-function Loggin() {
+
+function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -14,32 +18,68 @@ function Loggin() {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    // aca va la lógica de autenticación, por ejemplo, enviar los datos al servidor.
+  const handleLogin = (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente.
+
+    // Aquí debes agregar la lógica de autenticación, por ejemplo, enviar los datos al servidor.
     console.log('Email:', email);
     console.log('Password:', password);
   };
 
   return (
-    <div>
-      <h2>Iniciar sesión</h2>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input type="text" value={email} onChange={handleEmailChange} />
+  //   <div>
+  //    <AppBar>
+  //               <img class="logo" src={Logo} ></img>
+  //     </AppBar>
+  //   <div class="div">
+  //     <h3>BIENVENIDOS</h3>
+  //     <form onSubmit={handleLogin}>
+  //     <div class="caja">
+  //       <div>
+  //         <label>Email:</label>
+  //         <input type="text" placeholder="Email" value={email} onChange={handleEmailChange} />
+  //       </div>
+  //       <div>
+  //         <label>Contraseña:</label>
+  //         <input type="password" placeholder="Contraseña" value={password} onChange={handlePasswordChange} />
+  //       </div>
+  //     </div>
+  //       <button class="iniciar" type="submit">
+  //         Iniciar sesión
+  //       </button>
+  //       <button class="volver" type="submit">
+  //         Volver
+  //       </button>
+  //     </form>
+  //   </div>
+  // </div>
+  // );
+
+  <div className="wrapper fadeInDown">
+            <img class="logo" src={Logo} ></img>
+    
+      <div id="formContent">
+        <h2 className="active"> Sign In </h2>
+        <h2 className="inactive underlineHover">Sign Up </h2>
+
+        <div className="fadeIn first">
+          <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
         </div>
-        <div>
-          <label>Contraseña:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} />
+
+        <form>
+          <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" value={email} onChange={handleEmailChange}/>
+          <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" value={password} onChange={handlePasswordChange}/>
+          <input type="submit" className="fadeIn fourth" value="Log In" />
+        </form>
+        <div id="formFooter">
+          <a className="underlineHover" href="#">Forgot Password?</a>
+          <a className="underlineHover" href="#">Back</a>
         </div>
-        <button type="button" onClick={handleLogin}>
-          Iniciar sesión
-        </button>
-        <IconButton href="./">Volver</IconButton>
-      </form>
+      </div>
     </div>
   );
-
 }
 
-export default Loggin;
+export default Login;
+
+
